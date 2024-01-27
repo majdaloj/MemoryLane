30e78ce69a7be46ad519b8814874b1bd89b1db44
sms-sudo <irock12022@gmail.com>
2024-01-27 18:32:12 -0500

    main page setup temporary

--- mobile/components/Main.js
+++ mobile/components/Main.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, Image, TouchableOpacity, Header } from 'react-native';
+import Settings from './Settings.js';
+
+const Logo = () => (
+  <Image
+    source={require('../assets/memorylane_logo.png')}
+    style={{ width: 60, height: 20 }}
+  />
+);
+
+const SettingsIcon = ({ navigation }) => (
+  <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+    <Image
+      source={require('../assets/settings_icon.png')}
+      style={{ width: 30, height: 30, marginRight: 10 }} // Adjust the size and margin as needed
+    />
+  </TouchableOpacity>
+);
+
+const Stack = createStackNavigator();
 
 const Main = () => {
+  const imageSources = [
+    require('../assets/background1.png'),
+    require('../assets/background2.png'),
+    require('../assets/background3.png'),
+  ];
+  const [randomImageSource, setRandomImageSource] = useState(null);
+  useEffect(() => {
+    chooseRandomImage();
+  }, []);
+
+  const chooseRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * imageSources.length);
+    setRandomImageSource(imageSources[randomIndex]);
+  };
+
+
   return (
-    <View>
-      <Text>Main Page</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {randomImageSource && (
+        <Image
+          source={randomImageSource}
+          style={{ width: '100%', height: '100%', position: 'absolute' }}
+          resizeMode="cover"
+        />
+      )}
+
+      {/* <Header
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      /> */}
+
+
     </View>
   );
 }
