34bedc260cc139a1996beb842af41dc5c4980cb6
sms-sudo <irock12022@gmail.com>
2024-01-27 11:17:27 -0500

    blank page setup

---
+++ mobile/components/CreateMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CreateMessage = () => {
+  return (
+    <View>
+      <Text>CreateMessage Page</Text>
+    </View>
+  );
+}
+
+export default CreateMessage;
