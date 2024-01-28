1ee30e944bbb855b7ad35f6a04c1334b7b9f9313
Majda Lojpur <majda.lojpur@mail.utoronto.ca>
2024-01-27 20:44:59 -0500

    minor edits

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 // server.js
-const express = require("express");
-const sequelize = require("./db/sequelize");
-const User = require("./models/User");
-const Memory = require("./models/Memory");
-const UserBoundary = require("./models/UserBoundary");
+const express = require('express');
+const sequelize = require('./db/sequelize');
+const User = require('./models/User');
+const Memory = require('./models/Memory');
+const UserBoundary = require('./models/UserBoundary');
 
-const sendMemoryRouter = require("./routes/send-memory");
+const sendMemoryRouter = require('./routes/send-memory');
+
+const userRoutes = require('./routes/userRoutes');
+const memoryRoutes = require('./routes/memoryRoutes');
+const userBoundaryRoutes = require('./routes/boundaryRoutes');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -13,13 +17,13 @@ const port = process.env.PORT || 8080;
 const syncDatabase = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connected to the database.");
+    console.log('Connected to the database.');
 
     // Sync models with the database
     await sequelize.sync({ force: false });
-    console.log("Models synced with the database.");
+    console.log('Models synced with the database.');
   } catch (error) {
-    console.error("Error connecting to the database:", error);
+    console.error('Error connecting to the database:', error);
   }
 };
 
@@ -27,16 +31,21 @@ const syncDatabase = async () => {
 syncDatabase();
 
 // Ensure the database connection is closed gracefully on application termination
-process.on("SIGINT", () => {
-  console.log("Closing the database connection pool...");
+process.on('SIGINT', () => {
+  console.log('Closing the database connection pool...');
   sequelize.close().then(() => {
-    console.log("Database connection pool closed.");
+    console.log('Database connection pool closed.');
     process.exit(0);
   });
 });
 
 app.use(express.json());
-app.use("/schedule", sendMemoryRouter);
+app.use('/schedule', sendMemoryRouter);
+
+// Use the routes for endpoints
+app.use('/api/users', userRoutes);
+app.use('/api/memories', memoryRoutes);
+app.use('/api/user-boundaries', userBoundaryRoutes);
 
 // Start the server
 app.listen(port, () => {
